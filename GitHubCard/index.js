6d3d3b450d8f033bf9efd11f12bef98c6d6a56c5
vebradev <justinas@vebra.dev>
2019-09-05 22:56:34 +0300

    build followers cards using fetch data

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,82 +1,90 @@
-const data = axios
-  .get("https://api.github.com/users/vebradev")
+const name = "vebradev";
+
+axios
+  .get(`https://api.github.com/users/${name}`)
   .then(res => {
     const data = res.data;
-    
-    document
-    .querySelector(".cards")
-    .append(cardBuilder(data));
+    document.querySelector(".cards").append(cardBuilder(data));
   })
   .catch(err => {
     alert("Error: ", err.message);
   });
 
-
-/* Step 5: Now that you have your own card getting added to the DOM, either 
-          follow this link in your browser https://api.github.com/users/<Your github name>/followers 
-          , manually find some other users' github handles, or use the list found 
-          at the bottom of the page. Get at least 5 different Github usernames and add them as
-          Individual strings to the friendsArray below.
-          
-          Using that array, iterate over it, requesting data for each user, creating a new card for each
-          user, and adding that card to the DOM.
-*/
-
-const followersArray = [];
-
 function cardBuilder(data) {
   // parent container
-  let divCard = document.createElement('div');
-  divCard.classList.add('card');
+  let divCard = document.createElement("div");
+  divCard.classList.add("card");
 
   // image
-  let img = document.createElement('img');
+  let img = document.createElement("img");
   img.src = data.avatar_url;
 
   // info div
-  let divCardInfo = document.createElement('div');
-  divCardInfo.classList.add('card-info');
+  let divCardInfo = document.createElement("div");
+  divCardInfo.classList.add("card-info");
 
   // h3
-  let h3Name = document.createElement('h3');
-  h3Name.classList.add('name');
-  h3Name.textContent = data.name;
+  let h3Name = document.createElement("h3");
+  h3Name.classList.add("name");
+  h3Name.textContent = data.name ? data.name : `🤦‍`;
 
   // location paragraph
-  let pLocation = document.createElement('p');
-  pLocation.textContent = `Location: ${data.location}`;
+  let pLocation = document.createElement("p");
+  pLocation.textContent = data.location
+    ? `Location: ${data.location}`
+    : `Location: 🤦‍`;
 
   // profile paragraph
-  let pProfile = document.createElement('p');
+  let pProfile = document.createElement("p");
   pProfile.textContent = `Profile: `;
 
   // link in profile paragraph
-  let link = document.createElement('a');
+  let link = document.createElement("a");
   link.href = data.html_url;
   link.textContent = data.html_url;
 
   // folowers paragraphs
-  let pFollowers = document.createElement('p');
-  let pFollowing = document.createElement('p');
+  let pFollowers = document.createElement("p");
+  let pFollowing = document.createElement("p");
   pFollowers.textContent = `Folowers: ${data.followers}`;
   pFollowing.textContent = `Folowing: ${data.following}`;
 
   // bio paragraph
-  let pBio = document.createElement('p');
+  let pBio = document.createElement("p");
   pBio.textContent = data.bio ? `Bio: ${data.bio}` : `️Bio: 🤦‍`;
 
   // appends
   divCard.append(img, divCardInfo);
   pProfile.append(link);
-  divCardInfo.append(h3Name, pLocation, pProfile, pFollowers, pFollowing, pFollowing, pBio);
+  divCardInfo.append(
+    h3Name,
+    pLocation,
+    pProfile,
+    pFollowers,
+    pFollowing,
+    pFollowing,
+    pBio
+  );
 
   return divCard;
 }
 
-/* List of LS Instructors Github username's: 
-  tetondan
-  dustinmyers
-  justsml
-  luishrd
-  bigknell
-*/
+const followersArray = [
+  "borjasolerr",
+  "anuoluwa",
+  "krikaz",
+  "Becheru888",
+  "basilcea"
+];
+
+followersArray.forEach(name => {
+  axios
+    .get(`https://api.github.com/users/${name}`)
+    .then(res => {
+      const data = res.data;
+      document.querySelector(".cards").append(cardBuilder(data));
+    })
+    .catch(err => {
+      alert("Error: ", err.message);
+    });
+});
